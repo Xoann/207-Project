6aab62f1e650d86f3444690035c09587e5a6fca9
38336 <zhengjianxuan111@gmail.com>
2023-10-16 11:48:15 -0400

    GPTChatHelper 0.0.1

---
+++ src/test/java/com/gptchathelper/GptChatHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gptchathelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptChatHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
