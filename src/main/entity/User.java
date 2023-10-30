3dee7cf4f3b2b22465197d26c34bce60d1a89255
Alec Dong <alec.dong@mail.utoronto.ca>
2023-10-30 10:02:49 -0400

    added user interface

---
+++ src/main/entity/User.java
@@ -0,0 +1,23 @@
+package main.entity;
+
+import java.util.List;
+
+/**
+ * User is the interface that describes the entities that can partake in a conversation
+ */
+public interface User {
+
+    String getUsername();
+
+    String getPassword();
+
+    List<User> getFriends();
+
+    void setUsername(String username);
+
+    void setPassword(String password);
+
+    void addFriend(User user);
+
+    void removeFriend(User user);
+}
