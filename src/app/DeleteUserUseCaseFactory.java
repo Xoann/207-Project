4dfac7ba52f07a2fba3addec520b9772e4e92a63
FireNirva <zhengjianxuan111@gmail.com>
2023-12-01 22:29:04 -0500

    Add DeleteUserUseCaseFactory for deleteuser use case.

---
+++ src/app/DeleteUserUseCaseFactory.java
@@ -0,0 +1,52 @@
+package app;
+
+import data_access.FileUserDataAccessObject;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.delete_user.DeleteUserController;
+import interface_adapter.delete_user.DeleteUserPresenter;
+import interface_adapter.delete_user.DeleteUserViewModel;
+import interface_adapter.login.LoginViewModel;
+import use_case.delete_user.DeleteUserInputBoundary;
+import use_case.delete_user.DeleteUserInteractor;
+import use_case.delete_user.DeleteUserOutputBoundary;
+import view.DeleteUserView;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class DeleteUserUseCaseFactory {
+
+    private DeleteUserUseCaseFactory() {
+    }
+
+    public static DeleteUserView create(
+            ViewManagerModel viewManagerModel,
+            DeleteUserViewModel deleteUserViewModel,
+            LoginViewModel loginViewModel,
+            FileUserDataAccessObject userDataAccessObject) {
+
+     try {
+         DeleteUserController deleteUserController = createDeleteUserUseCase(viewManagerModel, deleteUserViewModel, loginViewModel, userDataAccessObject);
+         return new DeleteUserView(deleteUserViewModel, deleteUserController);
+    } catch (IOException e) {
+        JOptionPane.showMessageDialog(null, "Could not open user data file.");
+    }
+
+    return null;
+}
+
+private static DeleteUserController createDeleteUserUseCase(
+        ViewManagerModel viewManagerModel,
+        DeleteUserViewModel DeleteUserViewModel,
+        LoginViewModel loginViewModel,
+        FileUserDataAccessObject userDataAccessObject) throws IOException {
+
+    // Notice how we pass this method's parameters to the Presenter.
+    DeleteUserOutputBoundary deleteUserOutputBoundary = new DeleteUserPresenter(viewManagerModel, DeleteUserViewModel, loginViewModel);
+
+    DeleteUserInputBoundary deleteUserInteractor = new DeleteUserInteractor(
+            userDataAccessObject, deleteUserOutputBoundary);
+
+    return new DeleteUserController(deleteUserInteractor);
+}
+}
